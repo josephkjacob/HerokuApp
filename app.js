77638afe91e5e468e70bdcd854ceb7effa8ed143
joseph <josephkjacob@gmail.com>
2019-12-02 10:15:10 +0530

    added application and modules

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = new express();
+app.get("/", (req, res) =>{
+    res.send("WELCOME");
+})
+const port = proces.env.PORT || 3000;
+app.listen(port, (req, res)=>{
+    console.log("Listening port on ", port);
+});
